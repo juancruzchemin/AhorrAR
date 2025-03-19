982f1df1617af2dbe76924d7964736e836909bcd
juancruzchemin <juancruzcheminv@gmail.com>
2025-03-19 17:10:54 +0100

    Fixing

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000; // Cambia el puerto a 5000
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://ahorr-ar.vercel.app/', // Reemplaza con tu dominio de Vercel
+  credentials: true
+}));
 app.use(express.json());
 
 // Conectar a MongoDB
