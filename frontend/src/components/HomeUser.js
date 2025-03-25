a45e0f35ee4efe374f2a62ca9d66e08a5651e9f4
juancruzchemin <juancruzcheminv@gmail.com>
2025-03-25 17:40:51 +0100

    Adding responsive for HomeUsers

--- frontend/src/components/HomeUser.js
+++ frontend/src/components/HomeUser.js
@@ -10,7 +10,7 @@ const HomeUsers = () => {
           <h2>Bienvenido de nuevo!</h2>
         </header>
         <div className="home-buttons">
-          <h2>Gestión de Portafolios</h2>
+          <h2 className="section-title">Gestión de Portafolios</h2>
           <Link to="/portafolios" className="button portafolios-button">Ir a Portafolios</Link>
           <Link to="/inversiones" className="button inversiones-button">Ir a Inversiones</Link>
         </div>
